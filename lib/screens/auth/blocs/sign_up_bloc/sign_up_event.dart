ef03f385f5bbf0542617ab00451355d9df114b46
dioazril <63665371+dioazril@users.noreply.github.com>
2024-02-21 19:59:19 +0700

    added sign up bloc

--- lib/screens/auth/blocs/sign_up_bloc/sign_up_event.dart
+++ lib/screens/auth/blocs/sign_up_bloc/sign_up_event.dart
@@ -6,3 +6,16 @@ sealed class SignUpEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class SignUpRequired extends SignUpEvent {
+  final MyUser user;
+  final String password;
+
+  const SignUpRequired({
+    required this.user,
+    required this.password,
+  });
+
+  @override
+  List<Object> get props => [user, password];
+}
--- lib/screens/auth/blocs/sign_up_bloc/sign_up_state.dart
+++ lib/screens/auth/blocs/sign_up_bloc/sign_up_state.dart
@@ -2,9 +2,15 @@ part of 'sign_up_bloc.dart';
 
 sealed class SignUpState extends Equatable {
   const SignUpState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class SignUpInitial extends SignUpState {}
+
+final class SignUpSuccess extends SignUpState {}
+
+final class SignUpFailure extends SignUpState {}
+
+final class SignUpProcess extends SignUpState {}
