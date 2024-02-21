8f9d7e9c2a8eb2cc301de6cfe70a64e379e9b783
dioazril <63665371+dioazril@users.noreply.github.com>
2024-02-21 18:10:21 +0700

    updates my_app screen

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -3,6 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kofitek_app/blocs/auth_bloc/authentication_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
+import 'screens/auth/views/welcome.dart';
+import 'screens/home/views/home_screen.dart';
+import 'utils/shared_utils.dart';
+
 class MyApp extends StatelessWidget {
   final UserRepository userRepository;
   const MyApp(this.userRepository, {super.key});
@@ -13,7 +17,30 @@ class MyApp extends StatelessWidget {
       create: (context) => AuthenticationBloc(
         userRepository: userRepository,
       ),
-      child: Container(),
+      child: const MyAppView(),
+    );
+  }
+}
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Kofitek App',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: (context, state) {
+        if (state.status == AuthenticationStatus.authenticated) {
+          return const HomeScreen();
+        } else {
+          return const WelcomeScreen();
+        }
+      }),
     );
   }
 }
