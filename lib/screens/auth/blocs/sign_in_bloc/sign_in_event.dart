9c90d3cef7db4d0198c3085d54816416a53b8209
dioazril <63665371+dioazril@users.noreply.github.com>
2024-02-21 19:51:02 +0700

    added sign in bloc

--- lib/screens/auth/blocs/sign_in_bloc/sign_in_event.dart
+++ lib/screens/auth/blocs/sign_in_bloc/sign_in_event.dart
@@ -6,3 +6,18 @@ sealed class SignInEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class SignInRequired extends SignInEvent {
+  final String email;
+  final String password;
+
+  const SignInRequired({
+    required this.email,
+    required this.password,
+  });
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+class SignOutRequired extends SignInEvent {}
--- lib/screens/auth/blocs/sign_in_bloc/sign_in_state.dart
+++ lib/screens/auth/blocs/sign_in_bloc/sign_in_state.dart
@@ -2,9 +2,15 @@ part of 'sign_in_bloc.dart';
 
 sealed class SignInState extends Equatable {
   const SignInState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class SignInInitial extends SignInState {}
+
+final class SignInFailure extends SignInState {}
+
+final class SignInLoading extends SignInState {}
+
+final class SignInSuccess extends SignInState {}
