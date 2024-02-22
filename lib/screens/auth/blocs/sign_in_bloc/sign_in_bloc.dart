eb738ee1aeaf37ba95f5ef57c6aa697d35ec3744
dioazril <63665371+dioazril@users.noreply.github.com>
2024-02-22 21:57:36 +0700

    modified sign_in bloc

--- lib/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart
+++ lib/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart
@@ -13,6 +13,7 @@ class SignInBloc extends Bloc<SignInEvent, SignInState> {
       emit(SignInProcess());
       try {
         await _userRepository.signIn(event.email, event.password);
+        emit(SignInSuccess());
       } catch (e) {
         emit(SignInFailure());
       }
