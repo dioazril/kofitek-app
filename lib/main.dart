d4cb3b05f38557a38670fa3cb1379808ba13d3ee
dioazril <63665371+dioazril@users.noreply.github.com>
2024-02-22 17:05:04 +0700

    modified main.dart: added DefaultFirebaseOptions

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:kofitek_app/my_app.dart';
 import 'package:user_repository/user_repository.dart';
 import 'bloc_observer.dart';
+import 'utils/services/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = AppBlocObserver();
   runApp(MyApp(FirebaseUserRepository()));
 }
