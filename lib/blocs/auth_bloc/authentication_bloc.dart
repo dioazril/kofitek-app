4ce99cc219929f52ff02101889fd5fa5223fa2b6
dioazril <63665371+dioazril@users.noreply.github.com>
2024-02-22 21:56:58 +0700

    modified authentication_bloc

--- lib/blocs/auth_bloc/authentication_bloc.dart
+++ lib/blocs/auth_bloc/authentication_bloc.dart
@@ -7,24 +7,29 @@ import 'package:user_repository/user_repository.dart';
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
-class AuthenticationBloc
-    extends Bloc<AuthenticationEvent, AuthenticationState> {
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
   final UserRepository userRepository;
-  late final StreamSubscription<MyUser?> userSubscription;
+  late final StreamSubscription<MyUser?> _userSubscription;
 
   AuthenticationBloc({
-    required this.userRepository,
+    required this.userRepository
   }) : super(const AuthenticationState.unknown()) {
-    userSubscription = userRepository.user.listen((user) {
+    _userSubscription = userRepository.user.listen((user) {
       add(AuthenticationUserChanged(user));
     });
 
     on<AuthenticationUserChanged>((event, emit) {
-      if (event.user != MyUser.empty) {
+      if(event.user != MyUser.empty) {
         emit(AuthenticationState.authenticated(event.user!));
       } else {
         emit(const AuthenticationState.unauthenticated());
       }
     });
+  }
+
+  @override
+  Future<void> close() {
+    _userSubscription.cancel();
+    return super.close();
   }
 }
